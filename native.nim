5a4824e0fd509a29deddcc20cf56c0f810b5889f
Fabian Keller <github.100.fkeller@spamgourmet.com>
2017-01-03 22:40:13 +0100

    proper signature

--- native.nim
+++ native.nim
@@ -1,5 +1,6 @@
 import jnim
+#import jnim.private.jni_wrapper
 
-proc Java_NativeTest_run*(env: JNIEnvPtr, obj: jobject) {. cdecl, exportc, dynlib .} =
+proc Java_NativeTest_run*(env: JNIEnvPtr, class: jnim.jni_wrapper.jclass) {. cdecl, exportc, dynlib .} =
   system.setupForeignThreadGc()
   echo "Printing from JNI..."
